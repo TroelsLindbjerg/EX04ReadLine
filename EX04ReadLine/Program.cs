6b05bbb24f0e1eee2770f4467d0add3c70c6ab53
troe5906 <troe5906@CV-BB-5768>
2021-08-19 09:42:42 +0200

    Add project files.

---
+++ EX04ReadLine/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX04ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //opg4
+            /*
+            Console.WriteLine("Skriv noget her");
+            string userinput = Console.ReadLine();
+            Console.WriteLine(userinput);
+            */
+
+
+            /*
+            Console.WriteLine("Indtast Fornavn");
+            string Fornavn = Console.ReadLine();
+            Console.WriteLine("Indtast Efternavn");
+            string Efternavn = Console.ReadLine();
+            Console.Write("Velkommen ");
+            //($"{}{}"); Formatting hvis man vil have udskrivet flere ting sammen.
+            Console.WriteLine($"{Fornavn} {Efternavn}");
+            */
+
+
+            /*
+            Console.WriteLine("Indtast din alder");
+            //Brug .Parse for af kunde gemme/skrive tal.
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Betal med 99,95");
+            double price = double.Parse(Console.ReadLine());
+            */
+
+
+            /*
+            Console.WriteLine("Lad os udregne arealet på en firkant");
+            Console.WriteLine("Enter firste number");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int second = int.Parse(Console.ReadLine());
+            Console.Write("Arealet af firkanten er: ");
+            Console.WriteLine(first * second);
+            */
+
+            
+            Console.WriteLine("       AA");
+            Console.WriteLine("      A  A");
+            Console.WriteLine("     A    A");
+            Console.WriteLine("    AAAAAAAA");
+            Console.WriteLine("   A        A");
+            Console.WriteLine("  A          A");
+            
+
+
+
+        }
+    }
+}
